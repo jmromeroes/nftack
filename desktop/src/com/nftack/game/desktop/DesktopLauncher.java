aa6996e606ad0d7e64e511a7e28065c6fc51bb84
José Miguel Romero Espitia <90006571+jmromeroeci@users.noreply.github.com>
2021-11-24 23:53:11 -0500

    first commit

---
+++ desktop/src/com/nftack/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.nftack.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.nftack.game.Nftack;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Nftack(), config);
+	}
+}
